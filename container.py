20059a6e81d57cb54f6f5bac0068796bf297960a
dodonator <dodo@chaosdorf.de>
2023-01-31 12:53:22 +0100

    adds docstrings

--- container.py
+++ container.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Optional
 
 
 class Container:
@@ -7,18 +7,41 @@ class Container:
     current_index: int
 
     def __init__(self, size_limit: int):
+        """Container constructor.
+
+        Args:
+            size_limit (int): max capacity of container
+        """
         self.size_limit = size_limit
         self.inventory = []
         self.current_index = 0
 
     def __len__(self) -> int:
+        """Returns the current capacity.
+
+        Returns:
+            int: current capacity
+        """
         return len(self.inventory)
 
-    def __iter__(self) -> Iterator:
+    def __iter__(self) -> Iterator[str]:
+        """Iterates over the elements starting up top.
+
+        Yields:
+            Iterator[str]: element iterator
+        """
         for element in reversed(self.inventory):
             yield element
 
-    def __getitem__(self, index: int):
+    def __getitem__(self, index: int) -> Optional[str]:
+        """Returns element at given index or None.
+
+        Args:
+            index (int): index
+
+        Returns:
+            Optional[str]: element or None
+        """
         assert index in range(-self.size_limit, self.size_limit)
         try:
             element = self.inventory[index]
@@ -26,35 +49,77 @@ class Container:
             element = None
         return element
 
-    def add(self, element):
+    def add(self, element: str):
+        """Adds an element on top of the container.
+
+        Args:
+            element (str): element to add
+        """
         if not self.is_full():
             self.inventory.append(element)
             self.current_index += 1
 
-    def pop(self):
+    def pop(self) -> Optional[str]:
+        """Pops an element from the container.
+
+        Returns None if container is empty.
+
+        Returns:
+            Optional[str]: element or None
+        """
         if not self.is_empty():
             element = self.inventory.pop()
             self.current_index -= 1
             return element
+        else:
+            return None
 
     def is_empty(self) -> bool:
+        """Returns wether the container is empty.
+
+        Returns:
+            bool: result
+        """
         return not bool(self.inventory)
 
     def is_full(self) -> bool:
+        """Returns wether the maximum capacity is reached.
+
+        Returns:
+            bool: result
+        """
         return len(self) == self.size_limit
 
     def is_finished(self) -> bool:
+        """Returns  if the container is finished.
+
+        The container is finished if it is full
+        and all elements are of the same type.
+
+        Returns:
+            bool: result
+        """
         if self.is_full():
             return len(set(self)) == 1
         else:
             return False
 
-    def top(self):
+    def top(self) -> Optional[str]:
+        """Returns the top element or None, if empty.
+
+        Returns:
+            Optional[str]: top element
+        """
         if self.is_empty():
             return None
         return self[-1]
 
-    def bottom(self):
+    def bottom(self) -> Optional[str]:
+        """Returns the bottom element or None, if empty.
+
+        Returns:
+            Optional[str]: bottom element
+        """
         if self.is_empty():
             return None
         return self[0]
